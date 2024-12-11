4bf5c7ae3114c9bacffeafddff40e2f61952b666
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-11 15:22:01 +0530

    Add files via upload

---
+++ defaultconstructor.java
@@ -0,0 +1,20 @@
+class a{
+    int c;
+    String b;
+    a()
+    {
+        this.c=345;
+        this.b="hello";
+    }
+    void display()
+    {
+        System.out.println(b+" "+c);
+    }
+}
+public class defaultconstructor{
+    public static void main(String args[]){
+        a s=new a();
+        s.display();
+
+    }
+}
