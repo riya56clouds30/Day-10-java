95e3354eb9d263b20168b2ab3499b0ade602607a
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-11 15:25:05 +0530

    Add files via upload

---
+++ protectedinstatic.java
@@ -0,0 +1,9 @@
+class protectedinstatic{
+    protected static void s()
+    {
+        System.out.println("hello world");
+    }
+    public static void main(String args[]){
+      s();  
+    }
+  }
