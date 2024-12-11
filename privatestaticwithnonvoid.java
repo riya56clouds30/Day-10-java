70d8037cdb7fb164b49740abe8828a94e8b86201
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-11 15:24:40 +0530

    Add files via upload

---
+++ privatestaticwithnonvoid.java
@@ -0,0 +1,11 @@
+class privatestaticwithnonvoid{
+    private static String s()
+    {
+        return " hello priya";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
