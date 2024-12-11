640f8782c6d412e0a64310b33aff2414d66849b6
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-11 15:22:14 +0530

    Add files via upload

---
+++ exponential.java
@@ -0,0 +1,11 @@
+class exponential{
+    static int pow(int b,int ex){
+        if(ex==0)return 1;
+        return b*pow(b,ex-1);
+    }
+    public static void main(String args[])
+    {
+        int res=pow(7,8);
+        System.out.println(res);
+    }
+    }
