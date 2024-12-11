bac532a6f3ea0405bcda5aff782825770b99de2b
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-11 15:21:35 +0530

    Add files via upload

---
+++ constructoroverloading.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class constructoroverloading{
+    String name;
+    constructoroverloading()
+    {
+        name="rajesh";
+    }
+    constructoroverloading(String name)
+{
+    this.name=name;
+}
+
+void display()
+{
+    System.out.println(name);
+}
+public static void main(String args[]){
+constructoroverloading o=new constructoroverloading();
+o.display();
+constructoroverloading o1=new constructoroverloading("priya");
+o1.display();
+}
+}
