6038937a4b1abb38afa0522d7afc08b6bb2a9efe
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-12 10:27:13 +0530

    Add files via upload

---
+++ abstractex.java
@@ -0,0 +1,28 @@
+import java.util.*;
+abstract class mobileapp{
+  abstract void menu();
+  abstract void rating();
+  abstract void contact();
+}
+class b extends mobileapp
+{
+  public void menu()
+  {
+    System.out.println("Hello");
+  }
+  public void rating()
+  {
+    System.out.println("Hello Raju");
+  }
+  public void contact()
+  {
+    System.out.println("89898989879");
+  }
+}
+public class abstractex{
+  public static void main(String args[])
+  {
+    b a = new b();
+    a.rating();
+  }
+}
